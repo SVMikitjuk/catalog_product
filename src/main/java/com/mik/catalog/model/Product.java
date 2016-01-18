39f409a8cd706dcc3472debb015db7dd8644d56e
Микитюк Сергей Викторович <sergej.mikitjuk@privatbank.ua>
2016-01-18 11:18:02 +0200

    modify file for sql

--- src/main/java/com/mik/catalog/model/Product.java
+++ src/main/java/com/mik/catalog/model/Product.java
@@ -11,6 +11,7 @@ import java.io.Serializable;
 @Entity
 @Table(name = "product", schema = "", catalog = "addrBook")
 public class Product implements Serializable {
+
     private Integer id;
     private String name;
     @JsonProperty("group")
