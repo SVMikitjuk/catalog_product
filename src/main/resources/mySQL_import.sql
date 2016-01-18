82d3820633c8194458bf3444edcecb44bd8985d5
Микитюк Сергей Викторович <sergej.mikitjuk@privatbank.ua>
2016-01-18 11:18:55 +0200

    modify file for sql

---
+++ src/main/resources/mySQL_import.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO group_product (dept, name ) VALUES ('fruit', 'vegetables');
+INSERT INTO group_product (dept, name ) VALUES ('roots', 'vegetables');
+
+INSERT INTO product (name, id_group) VALUES ('apple',1);
+INSERT INTO product (name, id_group) VALUES ('apricot',1);
+
+INSERT INTO product (name, id_group) VALUES ('potato',2);
+INSERT INTO product (name, id_group) VALUES ('beet',2);
